e5628f88c04697f0be33916249469d8e35ddb1e1
Javi Monmany <monmany1992@gmail.com>
2023-01-03 13:30:20 +0100

    Fixed background-image and add generate button

--- src/features/pages/home/home.tsx
+++ src/features/pages/home/home.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import { Counter } from "../../../components/counter/counter";
 
 export default function Home () {
     return <>
         <div className="home-container" role="main">
             <Counter></Counter>
+            <Link to={"/robots"} className="material-symbols-outlined btn" title="Generate">precision_manufacturing</Link>
         </div>
     </>
 }
