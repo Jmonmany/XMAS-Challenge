20613906c8e74b8e5df57f0afb9f0ec616465eda
Javi Monmany <monmany1992@gmail.com>
2023-01-03 21:01:54 +0100

    Added Favourite Page testing

--- src/features/pages/bookmarks/bookmarks.tsx
+++ src/features/pages/bookmarks/bookmarks.tsx
@@ -12,22 +12,22 @@ export default function Bookmarks () {
     
     return (
         <>
-          <h2>Bookmarks</h2>
-          <ul className="robot-list">
-            {robots
-              .filter((item) => item.isFavourite)
-              .map((item) => {
-                return (
-                  <Robot
-                    key={item.id}
-                    item={item}
-                    handleUpdate={handleUpdate}
-                    handleDelete={handleDelete}
-                    handleFavourite={handleFavourite}
-                  ></Robot>
-                );
-              })}
-          </ul>
+            <h2>Bookmarks</h2>
+            <ul className="robot-list">
+                {robots
+                .filter((item) => item.isFavourite)
+                .map((item) => {
+                    return (
+                    <Robot
+                        key={item.id}
+                        item={item}
+                        handleUpdate={handleUpdate}
+                        handleDelete={handleDelete}
+                        handleFavourite={handleFavourite}
+                    ></Robot>
+                    );
+                })}
+            </ul>
         </>
       );
 }
