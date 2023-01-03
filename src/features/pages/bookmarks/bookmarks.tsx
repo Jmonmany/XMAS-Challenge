4fe316c3761d3b2535e90b0ace13aaa41e681324
Javi Monmany <monmany1992@gmail.com>
2023-01-03 18:03:07 +0100

    Added Favourite functionality

--- src/features/pages/bookmarks/bookmarks.tsx
+++ src/features/pages/bookmarks/bookmarks.tsx
@@ -1,5 +1,33 @@
+import { useEffect } from "react";
+import { Robot } from "../../../components/robot/robot";
+import { useRobots } from "../../../hooks/use.robots";
+
 export default function Bookmarks () {
-    return <>
-        <h2>Bookmarks</h2>
-    </>
+    const { robots, handleLoad, handleDelete, handleUpdate, handleFavourite } =
+        useRobots();
+
+    useEffect(() => {
+        handleLoad();
+    }, [handleLoad]);
+    
+    return (
+        <>
+          <h2>Bookmarks</h2>
+          <ul className="robot-list">
+            {robots
+              .filter((item) => item.isFavourite)
+              .map((item) => {
+                return (
+                  <Robot
+                    key={item.id}
+                    item={item}
+                    handleUpdate={handleUpdate}
+                    handleDelete={handleDelete}
+                    handleFavourite={handleFavourite}
+                  ></Robot>
+                );
+              })}
+          </ul>
+        </>
+      );
 }
